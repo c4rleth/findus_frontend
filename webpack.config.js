3a77ca9af04822cfa93d448e8d12b5e24a5bd6e9
Felipe Pitanga <felipe.pitanga@mainstreamit.net>
2022-11-19 18:25:54 -0300

    ultimas correções v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 // se eu não estiver em produção, eu estou em desenvolvimento
 const isDevelopment = process.env.NODE_ENV !== 'production'
@@ -29,6 +30,14 @@ module.exports = {
         isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'public/playground_assets'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
         })
     ].filter(Boolean),
     module: {
